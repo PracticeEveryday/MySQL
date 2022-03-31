1ccf7dce1fb0366d7ee842a6d656bb3d0ac56237
DongHyun Kim <developerforus@gmail.com>
2022-03-31 20:17:57 +0900

    .env add

--- src/db/Schema/user.js
+++ src/db/Schema/user.js
@@ -1,9 +1,11 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "111111",
+  password: process.env.mysql_key,
   database: "login_project",
 });
 
--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,9 @@ import { UserModel } from "./model/User";
 
 db.connect((err) => {
   if (err) {
-    console.log(err);
+    console.log("mysql connection error :" + err);
+  } else {
+    console.log("mysql is connected successfully.");
   }
 });
 
