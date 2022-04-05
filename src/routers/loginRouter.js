ecffdd666347fa7e701b5e45caa7adf537112e4b
DongHyun Kim <developerforus@gmail.com>
2022-04-05 22:16:40 +0900

    login update

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -6,6 +6,8 @@ const loginRouter = Router();
 
 loginRouter.route("/login").post(loginControl.getUser);
 
+loginRouter.route("/login/:id").put(loginControl.updateUser);
+
 // loginRouter.get("/login", (req, res) => {
 //   res.status(200).json({
 //     status: "succ",
