4b98e03f8b3822e1fd7c27cc72da44e73dd0256b
DongHyun Kim <developerforus@gmail.com>
2022-04-05 22:41:39 +0900

    file division

--- index.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./src/app";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`${PORT}번 포트 온`));
--- src/routers/registerRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { registerControl } from "../controller/registerControl";
-
-const registerRouter = Router();
-
-registerRouter.route("/register/create").post(registerControl.addUser);
-
-export { registerRouter };
