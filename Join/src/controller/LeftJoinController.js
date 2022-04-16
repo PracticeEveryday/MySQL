80392f7072859095be2b9e158d0b756dc05fd48a
DongHyun Kim <developerforus@gmail.com>
2022-04-16 15:23:27 +0900

    leftjoin modify

--- Join/src/controller/LeftJoinController.js
+++ Join/src/controller/LeftJoinController.js
@@ -3,27 +3,15 @@ import { db } from "../dbconfig";
 const LeftControler = {
   getLeft: async (req, res, next) => {
     try {
+      //SELECT * FROM author LEFT JOIN profile ON author.profile_id = profile.id;
+      //SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id;
       const { from, LeftJoin } = req.body;
       console.log(from, LeftJoin);
-      if (from === "topic" && LeftJoin === "author") {
-        const sql = `SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.aid;`;
-        db.query(sql, (error, results) => {
-          if (error) {
-            throw new Error(error);
-          }
-
-          res.status(200).json(results);
-        });
-      } else if (from == "author" && LeftJoin === "profile") {
-        const sql = `SELECT * FROM author LEFT JOIN profile ON author.profile_id = profile.pid;`;
-        db.query(sql, (error, results) => {
-          if (error) {
-            throw new Error(error);
-          }
-
-          res.status(200).json(results);
-        });
-      }
+      const sql = `SELECT * FROM ${from} LEFT JOIN ${LeftJoin} ON ${from}.${LeftJoin}_id = ${LeftJoin}.id`;
+      db.query(sql, (error, results) => {
+        if (error) throw new Error(error);
+        res.status(200).json(results);
+      });
     } catch (error) {
       next(error);
     }
