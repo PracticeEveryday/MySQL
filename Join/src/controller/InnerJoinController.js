045bf5d423ca653e5868783dd8d3ea85e31b4f2e
DongHyun Kim <developerforus@gmail.com>
2022-04-16 15:31:56 +0900

    inner

--- Join/src/controller/InnerJoinController.js
+++ Join/src/controller/InnerJoinController.js
@@ -3,6 +3,14 @@ import { db } from "../dbconfig";
 const InnerJoinController = {
   getInner: async (req, res, next) => {
     try {
+      // SELECT * FROM topic INNER JOIN author ON topic.author_id = author.id;
+      const { from, InnerJoin } = req.body;
+      console.log(from, InnerJoin);
+      const sql = `SELECT * FROM ${from} INNER JOIN ${InnerJoin} ON ${from}.${InnerJoin}_id = ${InnerJoin}.id;`;
+      db.query(sql, (error, results) => {
+        if (error) throw new Error(error);
+        res.status(200).json(results);
+      });
     } catch (error) {
       next(error);
     }
